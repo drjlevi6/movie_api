f8072e6192d3de3d054913918bc3539f1d5a208b
drjlevi6 <drjlevi6@aol.com>
2021-05-04 08:34:34 -0400

    Updated server.js
    Added comments and a console.log() call. Execution skips to console.log() after listen() and doesn't respond to input with a URL.

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const url = require('url');
 const http = require('http');
 
 http.createServer((request, response) => {
-  console.log('In createServer()\n');
+  console.log('In createServer()\n');   // NOT EXECUTED!
   response.writeHead(200, {'Content-Type': 'text/html'});
   response.end('Hello Node!\n');
 }).listen(8080);
 
-//console.log('My first Node test server is running on Port 8080.');
+console.log('My first Node test server is running on Port 8080.');  // Executed
 /*if(response.url.indexOf('html')>0) {
   console.log('request.url matches');
 } else {
