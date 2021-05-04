af23c2fc89b43883380e0e4d985afaf643e20447
drjlevi6 <drjlevi6@aol.com>
2021-05-03 23:42:49 -0400

    Update server.js
    Made various, unsuccessful attempts to have my createServer() call process requests.

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const http = require('http');
 
 http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello Node!\n');
+  //response.end('Hello Node!\n');
+  console.log('Request:', request);
+  console.log('Response:', response);
 }).listen(8080);
 
-console.log('My first Node test server is running on Port 8080.');
+/*console.log('My first Node test server is running on Port 8080.');
+if(response.url.indexOf('html')>0) {
+  console.log('request.url matches');
+} else {
+  console.log('URL doesn’t match')
+}*/
