53dcbc27e6bd3c73c72dba5b5dfbe9281cec47da
drjlevi6 <drjlevi6@aol.com>
2021-05-04 11:16:05 -0400

    Updated server.js; created index.html
    server.js's code now works. (Requires a call to the browser as well: "http://localhost:8080/", etc.
    index.html is a requirement of the current assignment.

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-const url = require('url');
+//const url = require('url');
 const http = require('http');
+const fs = require('fs');
 
 http.createServer((request, response) => {
   console.log('In createServer()\n');   // NOT EXECUTED!
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.end('Hello Node!\n');
+   console.log(request.url);
+  if(request.url.includes("documentation")) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream("documentation.html").pipe(response);
+  } else {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream("index.html").pipe(response);
+ }
 }).listen(8080);
 
-console.log('My first Node test server is running on Port 8080.');  // Executed
+console.log(
+	'My first Node test server is running on Port 8080.'
+);  // Executed
 /*if(response.url.indexOf('html')>0) {
   console.log('request.url matches');
 } else {
