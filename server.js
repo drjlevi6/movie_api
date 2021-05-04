6c2eb5e733faabe99ff773fb1fa7138a2da5b080
drjlevi6 <drjlevi6@aol.com>
2021-05-04 08:03:56 -0400

    Updated server.js
    in createServer(), changed {‘Content-Type’: ‘text/plain’} to {'Content-Type': 'text/html'}. This and many other changes failed. Seems that the callback function is getting skipped.

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const url = require('url');
 const http = require('http');
 
 http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  //response.end('Hello Node!\n');
-  console.log('Request:', request);
-  console.log('Response:', response);
+  console.log('In createServer()\n');
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.end('Hello Node!\n');
 }).listen(8080);
 
-/*console.log('My first Node test server is running on Port 8080.');
-if(response.url.indexOf('html')>0) {
+//console.log('My first Node test server is running on Port 8080.');
+/*if(response.url.indexOf('html')>0) {
   console.log('request.url matches');
 } else {
   console.log('URL doesn’t match')
